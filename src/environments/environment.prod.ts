aa543b0c7959debc17f55e29115548ca972df8be
AuraRenata812 <becaria.aura@mideservices.com>
2025-01-27 14:16:11 -0600

    Primer commit en el nuevo repositorio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,12 @@ export const environment = {
   production: true,
   youtubeApiKey: 'AIzaSyCmshCy6XXEsW1o78WyAoKQ5BAkTq9lTQE',
   firebase: {
-    apiKey: "AIzaSyAxPCxI5GeRFSwkFYgZi9ldb0Hzc8wlGcU",
-    authDomain: "consultasmedicas-807b5.firebaseapp.com",
-    databaseURL: "https://consultasmedicas-807b5-default-rtdb.firebaseio.com",
-    projectId: "consultasmedic  as-807b5",
-    storageBucket: "consultasmedicas-807b5.appspot.com",
-    messagingSenderId: "666909562096",
-    appId: "1:666909562096:web:923c5af177dc2ca54cde35",
-    measurementId: "G-EXMZB5W2T2"
+    apiKey: "AIzaSyBOFFZrO72Slf_LO2gw9xCsnvLCeUNkclo",
+    authDomain: "autolavado-1d518.firebaseapp.com",
+    projectId: "autolavado-1d518",
+    storageBucket: "autolavado-1d518.firebasestorage.app",
+    messagingSenderId: "408123920717",
+    appId: "1:408123920717:web:67c168fab3d4e8360aa704",
+    measurementId: "G-G4HWJ7CJDX"
   }
 };
